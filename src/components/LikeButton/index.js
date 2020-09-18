1bab8e16e379fb13546a8d732688d5211ba7a9ad
Manolis Sinadinakis <sinadinakis.e@gmail.com>
2020-09-18 12:21:27 +0200

    Add like button in movies with single and double click functionality

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -1,18 +1,47 @@
-import React from 'react';
-
-const ClickableBox = ({ onClick, onDoubleClick }) => (
-  <div style={{border: '1px solid white', borderRadius: '100px', padding: '10px', marginTop: '10px' }} onClick={onClick} onDoubleClick={onDoubleClick}>
-    Click or double click
-  </div>
-);
+import React, { useEffect, useState } from 'react';
 
 const LikeButton = () => {
-    return (
-        <ClickableBox
-            onClick={() => console.log("on click")}
-            onDoubleClick={() => console.log("on double click")}
-        />
-    )
+  const [clicks, setClicks] = useState(0);
+  const [totalClicks, setTotalClicks] = useState(0);
+  let total = 0;
+
+  useEffect(() => {
+    let singleClickTimer;
+    if (clicks === 1) {
+      singleClickTimer = setTimeout(
+        () => {
+          handleClick();
+          setClicks(0);
+        }, 250);
+    } else if (clicks === 2) {
+      handleDoubleClick();
+      setClicks(0);
+    }
+    return () => clearTimeout(singleClickTimer);
+  }, [clicks]);
+
+  const handleClick = () => {
+    console.log('single click');
+    total = totalClicks + 1;
+    setTotalClicks(total);
+  }
+
+  const handleDoubleClick = () => {
+    console.log('double click');
+    if (total > 0) {
+      total = totalClicks - 1;
+    }
+    setTotalClicks(total);
+  }
+
+  return (
+    <div
+      className="likeButton"
+      onClick={() => setClicks(clicks + 1)}
+    >
+      Likes | {totalClicks}
+    </div>
+  )
 }
 
 export default LikeButton;
--- src/components/ResultList/index.js
+++ src/components/ResultList/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './resultList.scss'
 
+import LikeButton from '../LikeButton';
+
 const ResultList = ({ movies }) => {
     return (
         <ul className="resultList">
             {movies && movies.map((movie, index ) => (
-                <li key="index">{movie.Title}</li>
+                <li key={`title-${index}`}>
+                    {movie.Title}
+                    <LikeButton />
+                </li>
             ))}
         </ul>
     )
